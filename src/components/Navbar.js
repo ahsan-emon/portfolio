5a838dc2e8d8e88014b3cc9b22a982e8136737e9
ahsan-emon <ahsan.emon23@gmail.com>
2022-11-17 02:45:02 +0600

    Navbar Completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,39 @@
-import React from "react"
-import logo from "../emon.png"
+import React from "react";
+import logo from "../emon.png";
+//REACT FONTAWESOME IMPORT
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <>
-        <nav className="navbar navbar-expand-lg bg-dark">
-  <div className="container"> 
-    <a className="navbar-brand" href="/"><img className="logo rounded-circle" src={logo} alt="logo" /></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Link</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+      <nav className="navbar navbar-expand-lg bg-dark">
+        <div className="container">
+          <a className="navbar-brand" href="/"><img className="logo rounded-circle" src={logo} alt="logo" /></a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">About me</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">How work</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">Portfolio</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">Contacts</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import bootstrap from '../node_modules/bootstrap/js/dist/collapse.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
