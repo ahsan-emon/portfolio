b41d9be66da196560fd9111edf87d28c027df8c8
Md. Ahsan Ullah Emon <55760395+ahsan-emon@users.noreply.github.com>
2022-11-16 14:02:07 +0600

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <>
+    <h2 className='text-secondary'>Portfolio Project </h2>
+    </>
+  );
+}
+
+export default App;
